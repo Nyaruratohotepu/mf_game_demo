5d6707c38edf80522238092d49554f47b3e39dfc
xuan <1853433873@qq.com>
2018-11-06 12:30:35 +0800

    自动生成地图1.0
    稍微完善怪物类；
    搭建怪物状态机；
    地图自动生成原理代码及示例已在test场景中实现，日后可再作调整

---
+++ MF_game_demo/Assets/Scripts/MapManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+    public GameObject cube;
+	// Use this for initialization
+	void Start () {
+        int[,] a = new int[3, 4] { { 1, 0, 1, 1 },     //地图信息
+                                   { 1, 1, 0, 1 },
+                                   { 1, 1, 1, 0 } };
+        createmap(a);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void createmap(int[,] a)
+    {
+        int i = 0, j = 0;
+        Vector3 pos = new Vector3(0, 0, 0);
+        while (i < a.GetLength(0))//第一遍铺地板
+        {
+            while (j < a.GetLength(1))
+            {
+                Instantiate(cube, pos, Quaternion.identity);
+                pos.x++;
+                j++;
+            }
+            j = 0;
+            i++;
+            pos.x = 0;
+            pos.z++;
+        }
+        i = 0;
+        j = 0;
+        pos = new Vector3(0, 1, 0);
+        while (i < a.GetLength(0))//第二遍垒墙
+        {
+            while (j < a.GetLength(1))
+            {
+                if (a[i, j] == 1)
+                {
+                    Instantiate(cube, pos, Quaternion.identity);
+                }
+                pos.x++;
+                j++;
+            }
+            j = 0;
+            i++;
+            pos.x = 0;
+            pos.z++;
+        }
+    }
+}
--- MF_game_demo/Assets/Scripts/Slime.cs
+++ MF_game_demo/Assets/Scripts/Slime.cs
@@ -21,9 +21,10 @@ public class Slime : MeleeMonster
         base.Target = null;
         base.HuntRange = 5;
         base.GameManager = GameObject.Find("GameManager").GetComponent<Manager>();
-        base.Player = GameObject.Find("Maruko") as GameObject;
+        base.Player = GameObject.Find("maruko") as GameObject;
         base.Self = self;
         base.CC = self.GetComponent<CharacterController>();
+        base.AC = self.GetComponent<Animator>();
     }
 
 
@@ -31,6 +32,7 @@ public class Slime : MeleeMonster
     {
         //播放攻击动画
         base.Attack();
+        
     }
 
     public override void Die()
