f5a281cc1407bb4a59a5ddec569bbd43d4a2df85
Huangxin <853522826@qq.com>
2018-12-23 22:25:03 +0800

    剧情对话
    重做地图
    剧情对话系统开头

---
+++ MF_game_demo/Assets/Scripts/Story/DataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Story
+{
+    public static class DataAccess
+    {
+        public static string storyFolderPath = "";
+        public static StorySection GetStorySection(string storyName)
+        {
+
+
+            return new StorySection();
+        }
+    }
+}
