b5da7accc216fa41989fd5af596ab671efce6040
Huangxin <853522826@qq.com>
2019-02-09 13:30:59 +0800

    配置文件注入路径字符串（未完成）

---
+++ MF_game_demo/Assets/Scripts/Story/Flag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Story
+{
+    public class Flag
+    {
+        private Dictionary<string, int> flags;
+    }
+}
