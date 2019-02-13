b8071b5c5b42904c1539d552e7d8a456ce24b810
Huangxin <853522826@qq.com>
2019-02-13 18:09:21 +0800

    Flag和Setting
    Flag和Setting文件加载和访问的逻辑
    均使用xml文件存储
    其中本项目中使用的路径信息统一存储在Setting.xml文件中

--- MF_game_demo/Assets/Scripts/Story/Flag.cs
+++ MF_game_demo/Assets/Scripts/Story/Flag.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
 
 namespace Assets.Scripts.Story
 {
-    public class Flag
+    public static class Flag
     {
-        private Dictionary<string, int> flags;
+        private static Dictionary<string, int> flags;
+        private static bool loaded = false;
+        private static void LoadFlag()
+        {
+            flags = new Dictionary<string, int>();
+            XmlDocument flagXml = new XmlDocument();
+
+            //获取flag文件地址，加载
+            flagXml.Load(Application.dataPath + Setting.Setting.GetPathValue("FlagFile"));
+            XmlNodeList flagNodelist = flagXml.SelectSingleNode("/Flags").ChildNodes;
+
+            foreach (XmlNode flag in flagNodelist)
+            {
+                string flagName = flag["FlagName"].InnerText;
+                int value;
+                if (!int.TryParse(flag["Value"].InnerText, out value)) break;
+                flags.Add(flagName, value);
+            }
+
+            loaded = true;
+        }
+
+        public static int GetValue(string FlagName)
+        {
+            if (!loaded) LoadFlag();
+            try
+            {
+                return flags[FlagName];
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+            
     }
 }
